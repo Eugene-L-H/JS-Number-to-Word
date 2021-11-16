7d1f5dc21f1f24134c1f73ee14e452b991896e32
Eugene Helland <eugenehelland@mail.com>
2021-11-16 15:19:49 -0700

    Added the ability to count numbers up to the ones section of quadrillions. Limited user  ability to enter numbers larger than 16 digits long. Remedied an error where I had forgot to a "billions" section in the block where numbers are pushed to their respective arrays.

--- numToWords.js
+++ numToWords.js
@@ -4,11 +4,18 @@
 // match the value of the first.
 
 function numToWords(num, start = num) {
+  const numLength = num.toString().length;
+  if (numLength > 15) {
+    console.log(
+      `A number of that size is too large. Must be 16 digits or less. Your number is ${numLength} digits long.`
+    );
+    return;
+  }
   // Used to reverse "num" in order to iterate backwards from the ones position
   function reverseString(str) {
-    const splStr = str.split("");
+    const splStr = str.split('');
     const revSpl = splStr.reverse();
-    const reverse = revSpl.join("");
+    const reverse = revSpl.join('');
     return reverse;
   }
 
@@ -19,19 +26,20 @@ function numToWords(num, start = num) {
     millions: [],
     billions: [],
     trillions: [],
+    quadrillions: [],
   };
 
   // This function generates a word based on each digit in the number and pushes
   // that word to numArrs
   function pushToArr(number, index, name) {
-    const plural = name + "s";
+    const plural = name + 's';
     const currentDigit = number[index];
 
     switch (true) {
       // Ones
       case index == 0 || index % 3 == 0:
         if (index == 0) {
-          if (currentDigit == "0") {
+          if (currentDigit == '0') {
             break;
           }
           numArrs[plural].push(ones[currentDigit]);
@@ -39,15 +47,15 @@ function numToWords(num, start = num) {
           // omit the place name if there are no numbers in the place value
           // group.
           if (
-            currentDigit == "0" &&
-            number[index + 1] == "0" &&
-            number[index + 2] == "0"
+            currentDigit == '0' &&
+            number[index + 1] == '0' &&
+            number[index + 2] == '0'
           ) {
             break;
           }
           numArrs[plural].push(name);
           // If currentDigit equals 0 nothing will be pushed.
-          if (currentDigit != "0") {
+          if (currentDigit != '0') {
             numArrs[plural].push(ones[currentDigit]);
           }
         }
@@ -55,11 +63,11 @@ function numToWords(num, start = num) {
 
       // Tens
       case index == 1 || (index - 1) % 3 == 0:
-        if (currentDigit == "0") {
+        if (currentDigit == '0') {
           break;
         }
         // Swap out word in ones position and replace with word from the 'teens' array.
-        if (currentDigit == "1") {
+        if (currentDigit == '1') {
           let ones = number[index - 1];
           if (numArrs[plural][ones] != name) {
             numArrs[plural].pop();
@@ -75,18 +83,18 @@ function numToWords(num, start = num) {
         if (index == 2) {
           // add 'and' after the ones and tens words in the 'hundreds' place
           // value group.
-          if (number[0] != "0" || number[1] != "0") {
-            numArrs[plural].push("and");
+          if (number[0] != '0' || number[1] != '0') {
+            numArrs[plural].push('and');
           }
-          if (currentDigit != "0") {
-            numArrs[plural].push("hundred");
+          if (currentDigit != '0') {
+            numArrs[plural].push('hundred');
             numArrs[plural].push(ones[currentDigit]);
           }
           break;
-        } else if (currentDigit == "0") {
+        } else if (currentDigit == '0') {
           break;
         } else {
-          numArrs[plural].push("hundred");
+          numArrs[plural].push('hundred');
           numArrs[plural].push(ones[currentDigit]);
         }
         break;
@@ -95,42 +103,42 @@ function numToWords(num, start = num) {
 
   // these 3 lists contain the words that will be indexed to match the digits
   const ones = [
-    "",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
+    '',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
   ];
 
   const teens = [
-    "ten",
-    "eleven",
-    "twelve",
-    "thirteen",
-    "fourteen",
-    "fifteen",
-    "sixteen",
-    "seventeen",
-    "eighteen",
-    "nineteen",
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
   ];
 
   const tens = [
-    "",
-    "ten",
-    "twenty",
-    "thirty",
-    "forty",
-    "fifty",
-    "sixty",
-    "seventy",
-    "eighty",
-    "ninety",
+    '',
+    'ten',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
   ];
 
   // iterate through num.
@@ -141,6 +149,7 @@ function numToWords(num, start = num) {
       millions: [],
       billions: [],
       trillions: [],
+      quadrillions: [],
     };
 
     let currNum = i.toString();
@@ -153,35 +162,47 @@ function numToWords(num, start = num) {
       switch (true) {
         // hundreds
         case place < 3:
-          pushToArr(revNum, place, "hundred");
+          pushToArr(revNum, place, 'hundred');
           break;
         // thousands
         case place < 6:
-          pushToArr(revNum, place, "thousand");
+          pushToArr(revNum, place, 'thousand');
 
           break;
         // millions
         case place < 9:
-          pushToArr(revNum, place, "million");
+          pushToArr(revNum, place, 'million');
           break;
+
+        // billions
+        case place < 12:
+          pushToArr(revNum, place, 'billion');
+          break;
+
         // trillions
+        case place < 15:
+          pushToArr(revNum, place, 'trillion');
+          break;
+
+        // quadrillion
         default:
-          pushToArr(revNum, place, "trillion");
+          pushToArr(revNum, place, 'quadrillion');
           break;
       }
       place++;
     }
 
     // Reverse contents of numArrs so words are in proper order.
-    let hund = numArrs["hundreds"].reverse();
-    let thou = numArrs["thousands"].reverse();
-    let mil = numArrs["millions"].reverse();
-    let bil = numArrs["billions"].reverse();
-    let tril = numArrs["trillions"].reverse();
+    let hund = numArrs['hundreds'].reverse();
+    let thou = numArrs['thousands'].reverse();
+    let mil = numArrs['millions'].reverse();
+    let bil = numArrs['billions'].reverse();
+    let tril = numArrs['trillions'].reverse();
+    let quad = numArrs['quadrillions'].reverse();
 
     // Move sub-arrays into one and combine contents.
-    let returnArr = [tril, bil, mil, thou, hund];
+    let returnArr = [quad, tril, bil, mil, thou, hund];
     let returnNum = returnArr.flat();
-    console.log(returnNum.join(" "));
+    console.log(returnNum.join(' '));
   }
 }
